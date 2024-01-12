ce429a40b9e225168cd86edc894048f356a2336e
Davidasdfa <davidkolar45@gmail.com>
2024-01-12 13:01:50 +0100

    dacia

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './source_files/logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function Nekaj(){
+  const [tekst, setTekst]=useState(true);
+  const tekst1="nekaj 1"
+  const tekst2="nekaj 2"
+
   return(
     <>
     <div id = 'neke'>Text v nekaj funkciji</div>
     <p>nekaj novega daciaaa</p>
+    <button onClick={(e)=>{setTekst(!tekst)}}>klikni me</button>
+    <span>{tekst? tekst1 : tekst2}</span>
     </>
   );
 
