05f41c84cd705b11cfa9b78bf0d3b67d27f124e8
unknown <davidkolar45@gmail.com>
2024-01-26 11:42:34 +0100

    susac-2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import logo from './source_files/logo.svg';
 import './App.css';
-import Neka from './fragment1/Neke.jsx';
+import Neke from './fragment1/Neke.jsx';
 import Frag from './fragment2/Frag.jsx';
 import Navbar from './navbar/navbar';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" component = {<Neke/>}/>
-        <Route path="/frag" component = {<Frag/>}/>
+        <Route path="/" element={<Neke />} />
+        <Route path="/frag" element={<Frag />} />
       </Routes>
 
     </div>
