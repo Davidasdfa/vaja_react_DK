e3180bbab3b7fc309b582b52c4b9736215bb9e69
David Kolar <davidkolar45@gmail.com>
2024-03-08 13:22:37 +0100

    .

--- src/fragment2/Frag.jsx
+++ src/fragment2/Frag.jsx
@@ -3,10 +3,11 @@ import {useEffect, useRef, useState} from 'react'
 
 
 function Frag(){
-  const [tekst, setTekst]=useState(false);
+  const [tekst, setTekst]=useState("");
   const reference=useRef(0);
   
   useEffect((e) => {
+    fetch("http://127.0.0.1:5000/books").then(y=>y.json()).then(x=>(setTekst(x))).catch(e=>{console.log(e)})
   }, [tekst])
 
   return (
